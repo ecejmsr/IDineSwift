00c39770f07da1c4bd4024fc997123ee57b3c8c5
Blue Parrot Software LLC <bluesoftwareparrotaws@gmail.com>
2022-04-02 00:31:42 -0500

    Initial Commit

---
+++ IDine/IDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  IDineApp.swift
+//  IDine
+//
+//  Created by Jose Rodriguez on 4/2/22.
+//
+
+import SwiftUI
+
+@main
+struct IDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
